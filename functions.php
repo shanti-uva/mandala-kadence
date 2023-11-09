d0348751b9ac5112f056dda963ff0105e8cfa48b
ThanGrove <than@virginia.edu>
2023-11-08 20:42:37 -0500

    adding update to mime types in functions.php (somehow inserted automatically?)

--- functions.php
+++ functions.php
@@ -5,3 +5,14 @@ require_once 'class-mandala-admin.php';
 
 /* Class to extend kadence to allow for subsite banners, menus, etc. */
 require_once 'class-mandala-kadence.php';
+
+ add_filter('upload_mimes', 'add_custom_upload_mimes');
+  function add_custom_upload_mimes($existing_mimes) {
+  	$existing_mimes['otf'] = 'application/x-font-otf';
+  	$existing_mimes['woff'] = 'application/x-font-woff';
+	$existing_mimes['woff2'] = 'application/x-font-woff';
+  	$existing_mimes['ttf'] = 'application/x-font-ttf';
+  	$existing_mimes['svg'] = 'image/svg+xml';
+  	$existing_mimes['eot'] = 'application/vnd.ms-fontobject';
+  	return $existing_mimes;
+  }
