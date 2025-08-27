5d41deb6a385bc8a4923e5e19d8a33f4c5ba5436
ThanGrove <than@virginia.edu>
2025-08-27 12:32:56 -0400

    update to sidebar and css for sitemap

--- sidebar.php
+++ sidebar.php
@@ -25,6 +25,13 @@ $options = get_option( 'mandala_plugin_options' );
 if ($options['default_sidebar'] * 1 == 0) {
     $closestyle = ' style="display: none;"';
 }
+
+$sitemap = '';
+
+if($options['sitemap_page_id']) {
+    $smid = $options['sitemap_page_id'];
+    $sitemap = get_post_field('post_content', $smid);
+}
 ?>
 <aside id="secondary" role="complementary" class="primary-sidebar widget-area <?php echo esc_attr( kadence()->sidebar_id_class() ); ?> sidebar-link-style-<?php echo esc_attr( kadence()->option( 'sidebar_link_style' ) ); ?>"<?php echo $closestyle; ?>>
 	<div class="sidebar-inner-wrap">
@@ -35,10 +42,28 @@ if ($options['default_sidebar'] * 1 == 0) {
 		do_action( 'kadence_before_sidebar' );
 
 		kadence()->display_sidebar();
+
+        if (!empty($sitemap)) {
+            ?>
+                <section id="sitemap" class="widget widget_nav_menu" style="display: none;">
+                    <h2 class="widget-title">Sitemap
+                        <div id="sitemap-close">
+                            <div id="sitemap-close-btn" style="">
+                                <button class="sitemap-header__closeButton"><span class="icon shanticon-cancel"></span></button>
+                            </div>
+                        </div>
+                    </h2>
+                    <div class="sitemap-container">
+                        <?php echo $sitemap; ?>
+                    </div>
+                </section>
+            <?php
+        }
 		/**
 		 * Hook for after sidebar.
 		 */
 		do_action( 'kadence_after_sidebar' );
+
 		?>
 	</div>
 </aside><!-- #secondary -->
